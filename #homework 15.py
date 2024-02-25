71c2aa81c061b93378da3a36d28d2ae3a247bf22
Lara Khachatryan <lara.khachatryan@sceon.am>
2024-02-25 23:29:00 +0400

    new changes homerowk15

---
+++ #homework 15.py
@@ -0,0 +1,11 @@
+# homework 15, ex 173
+# id n == "" chashxatec, cragir@ chi haskanum values() function@ 
+# def values():
+#     n = int(input("Please type a number, in case you want to finish, type 0: "))
+#     if n == "":
+#         return 0
+#     else:
+#         return n + values()
+# print(values())
+
+# ex 174
